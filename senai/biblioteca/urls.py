7a11aff431b907f5bff39258d9f5a982ca4f7b67
Nixx693 <nathaliafedoci@gmail.com>
2023-09-25 22:15:23 -0300

    links prontos

--- senai/biblioteca/urls.py
+++ senai/biblioteca/urls.py
@@ -6,4 +6,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("infantil.html", views.infantil, name="infantil"),
+    path("romance.html", views.romance, name="Romance"),
+    path("Fantasia.html", views.Fantasia, name="Fantasia"),
+    path("Fic.html", views.Fic, name="Fic"),
+    path("carrinho.html", views.carrinho, name="carrinho"),
+    path("index.html", views.index, name="index")
+
 ]
--- senai/biblioteca/views.py
+++ senai/biblioteca/views.py
@@ -4,4 +4,21 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render (request, 'index.html')
+    return render (request, 'index.html');
+
+def infantil(request):
+    return render (request, 'infantil.html')
+
+def romance(request):
+    return render (request, 'romance.html')
+
+def Fic(request):
+    return render (request, 'Fic.html')
+
+def Fantasia(request):
+    return render (request, 'Fantasia.html')
+
+def carrinho(request):
+    return render (request, 'carrinho.html')
+
+
