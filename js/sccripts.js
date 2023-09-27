8e1d60892c7b75c854197904f11e02317de1b5eb
Lrfaria <next313@hotmail.com>
2023-09-27 07:18:45 -0300

    html cor botao  modificados

---
+++ js/sccripts.js
@@ -0,0 +1,7 @@
+// script.js
+
+const loginButton = document.getElementById('signin');
+const cancelButton = document.getElementById('cancel');
+const loginForm = document.querySelector('.first-content .form');
+
+// Resto do código JavaScript
